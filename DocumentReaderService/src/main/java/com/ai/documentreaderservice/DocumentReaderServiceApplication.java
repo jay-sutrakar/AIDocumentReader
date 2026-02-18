47a6a007bf3eae1400ada42d6ba4d35c7468b688
jay-sutrakar <jaisutrakar@gmail.com>
2026-02-18 22:40:08 +0000

    added initiad directory for ui and service module

---
+++ DocumentReaderService/src/main/java/com/ai/documentreaderservice/DocumentReaderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ai.documentreaderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentReaderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentReaderServiceApplication.class, args);
+    }
+
+}
---
+++ DocumentReaderService/src/test/java/com/ai/documentreaderservice/DocumentReaderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.documentreaderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentReaderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
