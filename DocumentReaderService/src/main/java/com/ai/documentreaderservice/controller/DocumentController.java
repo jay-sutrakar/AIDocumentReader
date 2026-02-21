c24db19483a5c529326f06fe455ff11f406324cf
jay-sutrakar <jaisutrakar@gmail.com>
2026-02-21 13:26:24 +0000

    feat(doc-reader-service):added controller class and method to upload document

--- DocumentReaderService/src/main/java/com/ai/documentreaderservice/controller/DocumentController.java
+++ DocumentReaderService/src/main/java/com/ai/documentreaderservice/controller/DocumentController.java
@@ -1,22 +1,51 @@
 package com.ai.documentreaderservice.controller;
 
+import com.ai.documentreaderservice.model.UploadResponse;
+import com.ai.documentreaderservice.services.ChatService;
 import com.ai.documentreaderservice.services.RagService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
+import com.ai.documentreaderservice.services.UploadService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Slf4j
 @RestController
 @RequestMapping("/api/document")
 public class DocumentController {
-    private final RagService ragService;
-    DocumentController(RagService ragService) {
-        this.ragService = ragService;
+    private final UploadService uploadService;
+    private final ChatService chatService;
+    DocumentController(UploadService uploadService, ChatService chatService) {
+        this.chatService = chatService;
+        this.uploadService = uploadService;
     }
 
-    @GetMapping("/store")
-    String uploadDocument() {
-        ragService.storeDocument();
-        return "Hello";
+    @PostMapping("/upload")
+    public ResponseEntity<UploadResponse> uploadDocument(@RequestParam(value = "file", required = true) MultipartFile file, @RequestHeader(value = "userId", required = true) String userId) {
+        if (file.isEmpty()) {
+            return ResponseEntity.badRequest()
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .build();
+        }
+        try {
+            UploadResponse uploadResponse = uploadService.uploadDocument(file, userId);
+            log.info("logType=tracking | userId={} | sessionId={}", uploadResponse.getUserId(), uploadResponse.getSessionId());
+            return ResponseEntity
+                    .ok(uploadResponse);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError()
+                    .build();
+        }
+
     }
+    @GetMapping("/chat")
+    String chat() {
+        return chatService.getResponse();
+    }
+
 }
